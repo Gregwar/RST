a8c998e1ade22c31aa4cea324beb45d851d7dc16
Gregwar <g.passault@gmail.com>
2013-06-21 22:42:07 +0200

    Changing style of the demo

--- test/parse.php
+++ test/parse.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Demo</title>
+        <link rel="stylesheet" type="text/css" href="style.css" />
+    </head>
+<body>
 <?php
 
 include('../autoload.php');
@@ -8,3 +16,5 @@ $parser = new Parser;
 $document = $parser->parse(file_get_contents('document.rst'));
 
 echo $document;
+?>
+</body>
