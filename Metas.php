d1ea1b2fdd05dd8c00913dcc44f7cd84f97302fa
Gregwar <g.passault@gmail.com>
2013-07-02 23:47:12 +0200

    Adding method in metas

--- Metas.php
+++ Metas.php
@@ -6,6 +6,23 @@ class Metas
 {
     protected $entries = array();
 
+    /**
+     * Sets the meta for url, giving the title, the modification time and
+     * the dependencies list
+     */
+    public function set($url, $title, $mtime, array $depends)
+    {
+        $this->entries[$url] = array(
+            'url' => $url,
+            'title' => $title,
+            'mtime' => $mtime,
+            'depends' => $depends
+        );
+    }
+
+    /**
+     * Gets the meta for a given document reference url
+     */
     public function get($url)
     {
         if (isset($this->entries[$url])) {
