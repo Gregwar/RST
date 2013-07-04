638950338a230a6531ab3dbd7f94fb3294c50b36
Gregwar <g.passault@gmail.com>
2013-07-04 17:38:11 +0200

    Moving token replacing

--- HTML/Span.php
+++ HTML/Span.php
@@ -22,24 +22,6 @@ class Span extends Base
         // Emphasis
         $span = preg_replace('/\*\*(.+)\*\*/mUsi', '<b>$1</b>', $span);
         $span = preg_replace('/\*(.+)\*/mUsi', '<em>$1</em>', $span);
-
-        // Replacing literal tokens
-        foreach ($this->tokens as $id => $value) {
-            if ($value['type'] == 'literal') {
-                $span = str_replace($id, '<code>'.$value['text'].'</code>', $span);
-            }
-            if ($value['type'] == 'reference') {
-                $reference = $environment->resolve($value['url']);
-
-                if ($reference) {
-                    $text = $value['text'] ?: (isset($reference['title']) ? $reference['title'] : '');
-                    $link = '<a href="'.$reference['url'].'">'.trim($text).'</a>';
-                } else {
-                    $link = '<a href="#">(unresolved reference)</a>';
-                }
-                $span = str_replace($id, $link, $span);
-            }
-        }
         
         // Replacing variables
         $span = preg_replace_callback('/\|(.+)\|/mUsi', function($match) use ($environment) {
@@ -66,6 +48,24 @@ class Span extends Base
 
         // Adding brs when a space is at the end of a line
         $span = preg_replace('/ \n/', '<br />', $span);
+
+        // Replacing literal tokens
+        foreach ($this->tokens as $id => $value) {
+            if ($value['type'] == 'literal') {
+                $span = str_replace($id, '<code>'.$value['text'].'</code>', $span);
+            }
+            if ($value['type'] == 'reference') {
+                $reference = $environment->resolve($value['url']);
+
+                if ($reference) {
+                    $text = $value['text'] ?: (isset($reference['title']) ? $reference['title'] : '');
+                    $link = '<a href="'.$reference['url'].'">'.trim($text).'</a>';
+                } else {
+                    $link = '<a href="#">(unresolved reference)</a>';
+                }
+                $span = str_replace($id, $link, $span);
+            }
+        }
 
         return $span;
     }
