f4e0008b1fc1c82da9e988122caa6129237463e6
Gregwar <g.passault@gmail.com>
2013-09-29 20:21:21 +0200

    Commenting document directive

--- Directives/Document.php
+++ Directives/Document.php
@@ -8,7 +8,9 @@ use Gregwar\RST\Directive;
 use Gregwar\RST\Nodes\DocumentNode;
 
 /**
- * Tell that this document should be latex compilable
+ * Tell that this is a document, in the case of LaTeX for instance,
+ * this will mark the current document as one of the master document that
+ * should be compiled
  */
 class Document extends Directive
 {
