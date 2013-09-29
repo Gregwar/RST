27d1ee69293858c81058937ae2a6350b21cff5d0
Gregwar <g.passault@gmail.com>
2013-09-29 20:25:29 +0200

    README

--- test/document/demo.php
+++ test/document/demo.php
@@ -1,24 +1,10 @@
-RST
-===
-
-PHP library to parse reStructuredText document
-
-*NOTE: This is a work in progress, the library is not finished at all and then
-not documented, if you want to contribute or bring idea, don't hesitate to post
-me a message or on the issues tracking*
-
-Usage
------
-
-The parser can be used this way:
-
-```php
 <?php
+
+include('../../autoload.php');
 
 $parser = new Gregwar\RST\Parser;
 
-// RST document
-$rst = ' 
+$rst = '
 Hello world
 ===========
 
@@ -33,10 +19,8 @@ Where can I get it?
 You can get it on the `GitHub page <https://github.com/Gregwar/RST>`_
 ';
 
-// Parse it
 $document = $parser->parse($rst);
 
-// Render it
 echo $document->render();
 /* Will output:
 <a id="title.1"></a><h1>Hello world</h1>
@@ -45,12 +29,3 @@ echo $document->render();
 <a id="title.1.2"></a><h2>Where can I get it?</h2>
 <p>You can get it on the <a href="https://github.com/Gregwar/RST">GitHub page</a></p>
 */
-```
-
-For more information, you can have a look at `test/document/document.rst` and its result
-`test/document/document.html`
-
-License
--------
-
-This library is under MIT license
